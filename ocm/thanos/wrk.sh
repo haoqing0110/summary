c690d632580483478b34a6d64c51b4d3dcf23f2f
haoqing0110 <qhao@redhat.com>
2021-06-24 08:05:02 +0000

    update concurrency case

--- ocm/thanos/wrk.sh
+++ ocm/thanos/wrk.sh
@@ -1,14 +1,49 @@
 #/bin/bash
-DOMAIN="http://observability-thanos-query-frontend.open-cluster-management-observability.svc:9090"
+#DOMAIN="https://multicloud-console.apps.obs-china-aws-4616-64sjs.dev05.red-chesterfield.com/observability-query"
+#HEAD="Authorization: Bearer sha256~fg4alaJ3_z-kw5zNm0_RxwiW-piM-6R-k0nExwlWqRU"
+DOMAIN="${DOMAIN:-http://observability-thanos-query-frontend.open-cluster-management-observability.svc:9090}"
 
-wrk -t1 -c1 -d15m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
-wrk -t10 -c10 -d15m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
-wrk -t100 -c100 -d15m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+if [[ -z "${HEAD}" ]]; then
+  curl ${DOMAIN}/api/v1/query\?query\=count\(workqueue_depth\)
+  # raw data
+  wrk -t1 -c1 -d10m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  wrk -t5 -c5 -d10m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  wrk -t10 -c10 -d10m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  
+  # filter
+  wrk -t1 -c1 -d10m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  wrk -t5 -c5 -d10m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  wrk -t10 -c10 -d10m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  
+  # aggregation hourly
+  wrk -t1 -c1 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
+  wrk -t5 -c5 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
+  wrk -t10 -c10 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
 
-wrk -t1 -c1 -d15m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace="default"}"
-wrk -t10 -c10 -d15m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace="default"}"
-wrk -t100 -c100 -d15m --latency "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace="default"}"
+  # aggregation daily
+  wrk -t1 -c1 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+  wrk -t5 -c5 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+  wrk -t10 -c10 -d10m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+else 
+  curl -k -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=count\(workqueue_depth\)
+  # raw data
+  wrk -t1 -c1 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  wrk -t5 -c5 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  wrk -t10 -c10 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  
+  # filter
+  wrk -t1 -c1 -d10m --latency -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  wrk -t5 -c5 -d10m --latency -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  wrk -t10 -c10 -d10m --latency -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
+  
+  # aggregation daily
+  wrk -t1 -c1 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
+  wrk -t5 -c5 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
+  wrk -t10 -c10 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
 
-wrk -t1 -c1 -d15m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
-wrk -t10 -c10 -d15m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
-wrk -t100 -c100 -d15m --latency ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+  # aggregation hourly
+  wrk -t1 -c1 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+  wrk -t5 -c5 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+  wrk -t10 -c10 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
+fi
+
