9228b94ed22de2840c23b3f74716b92ab46f9bd4
haoqing0110 <qhao@redhat.com>
2021-07-01 09:59:36 +0000

    update filter

--- ocm/thanos/wrk.sh
+++ ocm/thanos/wrk.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-DOMAIN="https://multicloud-console.apps.obs-china-aws-4616-64sjs.dev05.red-chesterfield.com/observability-query"
-HEAD="Authorization: Bearer sha256~lVh8Jqq8ytq6f_pItMBMltFjKLYz4br5Y1khArkSr6c"
-#DOMAIN="${DOMAIN:-http://observability-thanos-query-frontend.open-cluster-management-observability.svc:9090}"
+#DOMAIN="https://multicloud-console.apps.obs-china-aws-4616-64sjs.dev05.red-chesterfield.com/observability-query"
+#HEAD="Authorization: Bearer sha256~QlVYe36mpe1yLWY5XoM1aTizAAPqtNoIYKpJeCLhsAw"
+DOMAIN="${DOMAIN:-http://observability-thanos-query-frontend.open-cluster-management-observability.svc:9090}"
 
 if [[ -z "${HEAD}" ]]; then
   curl ${DOMAIN}/api/v1/query\?query\=count\(workqueue_depth\)
@@ -10,11 +10,18 @@ if [[ -z "${HEAD}" ]]; then
   wrk -t5 -c5 -d10m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
   wrk -t10 -c10 -d10m --latency ${DOMAIN}/api/v1/query\?query\=workqueue_depth
   
-  # filter
-  wrk -t1 -c1 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  wrk -t5 -c5 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  wrk -t10 -c10 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  
+  # filter with query
+  wrk -t1 -c1 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-g1(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-g2(.*)\"}"
+  wrk -t10 -c10 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+
+  # filter with series
+  wrk -t1 -c1 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t10 -c10 -d10m --latency --timeout 20s "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+
   # aggregation hourly
   wrk -t1 -c1 -d10m --latency --timeout 20s ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
   wrk -t5 -c5 -d10m --latency --timeout 20s ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
@@ -27,15 +34,22 @@ if [[ -z "${HEAD}" ]]; then
 else 
   curl -k -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=count\(workqueue_depth\)
   # raw data
-  wrk -t1 -c1 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
-  wrk -t5 -c5 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
-  wrk -t10 -c10 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=workqueue_depth
+  wrk -t1 -c1 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query?query=workqueue_depth
+  wrk -t5 -c5 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query?query=workqueue_depth
+  wrk -t10 -c10 -d10m --latency -H "$HEAD" ${DOMAIN}/api/v1/query?query=workqueue_depth
   
-  # filter
-  wrk -t1 -c1 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  wrk -t10 -c10 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{namespace=\"default\"}"
-  
+  # filter with query
+  wrk -t1 -c1 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-g1(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-g2(.*)\"}"
+  wrk -t10 -c10 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/query?query=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+
+  # filter with series
+  wrk -t1 -c1 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+  wrk -t10 -c10 -d10m --latency --timeout 20s -H "$HEAD" "${DOMAIN}/api/v1/series?match[]=workqueue_depth{cluster=~\"simulate-(.*)\"}"
+
   # aggregation daily
   wrk -t1 -c1 -d10m --latency --timeout 20s -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
   wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[60m\]\)
@@ -46,4 +60,3 @@ else
   wrk -t5 -c5 -d10m --latency --timeout 20s -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
   wrk -t10 -c10 -d10m --latency --timeout 20s -H "$HEAD" ${DOMAIN}/api/v1/query\?query\=rate\(workqueue_depth\[24h\]\)
 fi
-
