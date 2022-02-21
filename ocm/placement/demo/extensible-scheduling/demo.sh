69f10ac3883d01b8abfaeda6d2dc9f22fe3c5027
haoqing0110 <qhao@redhat.com>
2022-02-21 03:21:44 +0000

    add clustertool scoretool
    Signed-off-by: haoqing0110 <qhao@redhat.com>

--- ocm/placement/demo/extensible-scheduling/demo.sh
+++ ocm/placement/demo/extensible-scheduling/demo.sh
@@ -28,7 +28,7 @@ DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 # clean env
 NS="demo1"
 #kubectl delete ns $(oc get managedcluster | awk '{print $1}')
-KUBECONFIG="/root/kind.config"
+KUBECONFIG="/root/.kube/config"
 scoretool --clean --resource-name demo1
 scoretool --clean --resource-name demo2
 kubectl delete managedcluster $(oc get managedcluster | awk '{print $1}')
@@ -50,7 +50,7 @@ p "Precondition 1: The environment has 3 managed clusters."
 pe "kubectl get managedclusters"
 
 p "Precondition 2: A controller is maintaining the AddOnPlacementScore CRs for the 3 clusters."
-p "scoretool --resource-name demo1 --interval 30"
+p "scoretool --resource-name demo1 --interval 20"
 pe "kubectl get AddOnPlacementScore -A"
 pe "kubectl get AddOnPlacementScore -A -oyaml"
 pe "kubectl get addonplacementscore -A -o=jsonpath='{range .items[*]}{.metadata.namespace}{\"\\t\"}{.status.scores}{\"\\n\"}{end}'"
@@ -60,7 +60,7 @@ pe "cat demo1-1.yaml"
 pe "kubectl create -f demo1-1.yaml -n ${NS}"
 pe "kubectl describe -f demo1-1.yaml -n ${NS} | grep Events -A 10"
 pe "kubectl describe placementdecision demo1-1-decision-1 -n ${NS} | grep Status -A 10"
-p "Placement reschedule every 5 minutes (in this demo is 30s)"
+p "Placement reschedule every 5 minutes (in this demo is 20s)"
 cmd
 cmd
 cmd
@@ -75,7 +75,6 @@ cmd
 p "Case 2: I want to select 2 clusters with highest customized score cpuratio and keep the decision steady."
 pe "cat demo1-2.yaml"
 pe "kubectl create -f demo1-2.yaml -n ${NS}"
-pe "kubectl get -f demo1-2.yaml -n ${NS} -oyaml"
 pe "kubectl describe -f demo1-2.yaml -n ${NS} | grep Events -A 10"
 pe "kubectl describe placementdecision demo1-2-decision-1 -n ${NS} | grep Status -A 10"
 cmd
