5864bc5f4a5ad9877dc8dc138c54f638b799a268
haoqing0110 <qhao@redhat.com>
2022-03-07 08:05:00 +0000

    placement taint/tolerations demo
    Signed-off-by: haoqing0110 <qhao@redhat.com>

--- ocm/placement/demo/taint-toleration/demo.sh
+++ ocm/placement/demo/taint-toleration/demo.sh
@@ -29,104 +29,67 @@ DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 NS="demo1"
 #kubectl delete ns $(oc get managedcluster | awk '{print $1}')
 KUBECONFIG="/root/.kube/config"
-scoretool --clean --resource-name demo1
-scoretool --clean --resource-name demo2
-kubectl delete managedcluster $(oc get managedcluster | awk '{print $1}')
-kubectl delete -f  ./ -n demo1
-kubectl delete -f  ./ -n demo2
+kubectl delete managedcluster $(kubectl get managedcluster | awk '{print $1}')
+kubectl delete placement -n ${NS} $(kubectl get placement -n ${NS} | awk '{print $1}')
 
 # hide the evidence
 kubectl create ns ${NS}
 kubectl create ns cluster1
 kubectl create ns cluster2
 kubectl create ns cluster3
-clustertool --create -c cluster1 -n ${NS}
-clustertool --create -c cluster2 -n ${NS}
-clustertool --create -c cluster3 -n ${NS}
 
-clear
-p "Placment extensible scheduling demo. Users could use scores in AddOnPlacementScore to select clusters."
-p "Precondition 1: The environment has 3 managed clusters."
+kubectl apply -f cluster-maintaining/cluster1.yaml
+kubectl apply -f cluster-maintaining/cluster2.yaml
+clear 
+p "Case 1: Cluster Maintaining."
+p "User can set a cluster to maintaining status, no workloads will be scheduled to the cluster."
 pe "kubectl get managedclusters"
+pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec}{\"\\n\"}{end}'"
+pe "cat cluster-maintaining/placement1.yaml"
+pe "kubectl apply -f cluster-maintaining/placement1.yaml -n ${NS}"
+pe "kubectl get placement -n demo1"
+pe "kubectl get placementdecision -n demo1"
+p ""
 
-p "Precondition 2: A controller is maintaining the AddOnPlacementScore CRs for the 3 clusters."
-p "scoretool --resource-name demo1 --interval 20"
-pe "kubectl get AddOnPlacementScore -A"
-pe "kubectl get AddOnPlacementScore -A -oyaml"
-pe "kubectl get addonplacementscore -A -o=jsonpath='{range .items[*]}{.metadata.namespace}{\"\\t\"}{.status.scores}{\"\\n\"}{end}'"
-
-p "Case 1: I want to select 2 clusters with highest customized score cpuratio."
-pe "cat demo1-1.yaml"
-pe "kubectl create -f demo1-1.yaml -n ${NS}"
-pe "kubectl describe -f demo1-1.yaml -n ${NS} | grep Events -A 10"
-pe "kubectl describe placementdecision demo1-1-decision-1 -n ${NS} | grep Status -A 10"
-p "Placement reschedule every 5 minutes (in this demo is 20s)"
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-
-p "Case 2: I want to select 2 clusters with highest customized score cpuratio and keep the decision steady."
-pe "cat demo1-2.yaml"
-pe "kubectl create -f demo1-2.yaml -n ${NS}"
-pe "kubectl describe -f demo1-2.yaml -n ${NS} | grep Events -A 10"
-pe "kubectl describe placementdecision demo1-2-decision-1 -n ${NS} | grep Status -A 10"
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-cmd
-
-p "Case 3: I want to select 1 cluster with lowest customized score cpuratio."
-pe "cat demo1-3.yaml"
-pe "kubectl create -f demo1-3.yaml -n ${NS}"
-pe "kubectl describe -f demo1-3.yaml -n ${NS} | grep Events -A 10"
-pe "kubectl describe placementdecision demo1-3-decision-1 -n ${NS} | grep Status -A 10"
-
-pe "- end -"
-
+kubectl apply -f schedule-on-certain/cluster1.yaml
+kubectl apply -f schedule-on-certain/cluster2.yaml
+kubectl apply -f schedule-on-certain/cluster3.yaml
 clear
-#kubectl delete managedcluster $(oc get managedcluster | awk '{print $1}')
-#kubectl delete -f  ./ -n ${NS}
-#NS="demo2"
-#kubectl delete -f  ./ -n ${NS}
-#kubectl create ns ${NS}
-#kubectl create ns primary
-#kubectl create ns backup
-#clustertool --create -c primary -n ${NS}
-#clustertool --create -c backup -n ${NS}
-#
-#clear
-#p "Case 2: Disaster recovery workload could be automatically switched to an avaliable cluster."
-#p "The environment has 2 managed clusters, one is primary and one is backup."
-#pe "kubectl get managedclusters"
-#
-#p "A controller is maintaining the AddOnPlacementScore CRs for the 2 clusters."
-#p "scoretool --resource-name demo2 --clusters primary,backup --scores 100,1 --interval 30"
-#
-#pe "kubectl get AddOnPlacementScore -A"
-#pe "kubectl get AddOnPlacementScore -A -oyaml"
-#
-#p "I want my workload running one the primary cluster."
-#pe "cat demo2.yaml"
-#pe "kubectl create -f demo2.yaml -n ${NS}"
-#pe "kubectl describe -f demo2.yaml -n ${NS}"
-#pe "kubectl describe placementdecision demo2 -n ${NS}"
-#
-#p "Primary cluster is down."
-#pe "kubectl label clusters"
-#
-#p "My workload switched to backup automatically."
-#pe "kubectl describe placementdecision demo2 -n ${NS}"
-#
+p "Case 2: Schedule on Certain Clusters."
+p "Users schedule some workloads to certain managed clusters."
+pe "kubectl get managedclusters"
+pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec}{\"\\n\"}{end}'"
+pe "kubectl get placement -n demo1"
+pe "kubectl get placementdecision -n demo1"
+p "Adding tolerations to placement"
+pe "cat schedule-on-certain/placement1-new.yaml"
+pe "kubectl apply -f schedule-on-certain/placement1-new.yaml -n ${NS}"
+pe "kubectl get placement -n demo1"
+pe "kubectl get placementdecision -n demo1"
+pe "kubectl describe placementdecision demo1-decision-1 -n ${NS} | grep Status -A 10"
+p ""
+
+kubectl delete -f schedule-on-certain/placement1-new.yaml
+kubectl apply -f eviction/cluster1.yaml
+kubectl apply -f eviction/cluster2.yaml
+kubectl apply -f eviction/cluster3.yaml
+clear
+p "Case 3: Immediate Eviction."
+p "When a managed cluster gets offline, the system can make applications deployed on this cluster to be transferred to another available managed cluster immediately."
+pe "kubectl get managedclusters"
+pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec}{\"\\n\"}{end}'"
+pe "cat eviction/placement3.yaml"
+pe "kubectl apply -f eviction/placement3.yaml -n ${NS}"
+pe "kubectl get placement -n demo1"
+pe "kubectl get placementdecision -n demo1"
+pe "kubectl describe placementdecision demo3-decision-1 -n ${NS} | grep Status -A 10"
+selected=$(kubectl get placementdecision demo3-decision-1 -n demo1  -o=jsonpath='{.status.decisions[0].clusterName}')
+git checkout eviction/cluster.yaml
+sed -i "s/PLACEHOLDER/${selected}/g" eviction/cluster.yaml
+p "Adding taints to ${selected}"
+pe "cat eviction/cluster.yaml"
+pe "kubectl apply -f eviction/cluster.yaml -n ${NS}"
+pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec}{\"\\n\"}{end}'"
+pe "kubectl describe placementdecision demo3-decision-1 -n ${NS} | grep Status -A 10"
+p ""
+clear
