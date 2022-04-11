1230183b590467e5d992aa3a0de1d6b5a022a3e6
haoqing0110 <qhao@redhat.com>
2022-04-11 04:07:52 +0000

    update taint-toleration demo
    Signed-off-by: haoqing0110 <qhao@redhat.com>

--- ocm/placement/demo/taint-toleration/playback.sh
+++ ocm/placement/demo/taint-toleration/playback.sh
@@ -26,43 +26,45 @@ DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 # DEMO_CMD_COLOR=$BLACK
 
 # clean env
-NS="demohq"
-c1="cluster1"
-c2="cluster2"
+NS="demo1"
+C1="cluster1"
+C2="cluster2"
 #kubectl delete ns $(oc get managedcluster | awk '{print $1}')
 KUBECONFIG="/root/.kube/config"
 kubectl delete placement -n ${NS} $(kubectl get placement -n ${NS} | awk '{print $1}')
-kubectl delete managedcluster $(oc get managedcluster | awk '{print $1}')
+#kubectl patch managedcluster ${C1} -p '{"spec":{"taints":[{"effect":"NoSelect","key":"gpu","value":"true"}]}}' --type=merge
+#kubectl patch managedcluster ${C2} -p '{"spec":{"taints":[{"effect":"NoSelect","key":"gpu","value":"true"}]}}' --type=merge
 
 kubectl create ns ${NS}
-clustertool --create -c ${c1} -n ${NS}
-clustertool --create -c ${c2} -n ${NS}
-kubectl patch managedcluster ${c1} -p '{"spec":{"taints":[{"effect":"NoSelect","key":"cluster.open-cluster-management.io/unreachable"}]}}' --type=merge
-kubectl patch managedcluster ${c2} -p '{"spec":{"taints":[{"effect":"NoSelect","key":"cluster.open-cluster-management.io/unreachable"}]}}' --type=merge
+clusteradm create clusterset ${NS}
+clusteradm clusterset add ${NS} --clusters ${C1}
+clusteradm clusterset add ${NS} --clusters ${C2}
+clusteradm clusterset bind ${NS} --namespace ${NS}
+
+#clear 
+#p "Case 1: DO NOT schedule workload to clusters with taint."
+#pe "kubectl get managedclusters"
+#pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec.taints}{\"\\n\"}{end}'"
+#pe "cat placement1.yaml"
+#pe "kubectl apply -f placement1.yaml -n ${NS}"
+#pe "kubectl get placement -n ${NS}"
+#pe "kubectl get placementdecision -n ${NS}"
+#p ""
+#
+#p "Case 2: User could use toleration to schedule workload to clusters with taint."
+#pe "cat placement1-toleration.yaml"
+#pe "kubectl apply -f placement1-toleration.yaml -n ${NS}"
+#pe "kubectl get placement -n ${NS}"
+#pe "kubectl get placementdecision -n ${NS}"
+#pe "kubectl describe placementdecision demo1-decision-1 -n ${NS} | grep Status -A 10"
+#p ""
+
+kubectl patch managedcluster ${C1} -p '{"spec":{"taints":[]}}' --type=merge
+kubectl patch managedcluster ${C2} -p '{"spec":{"taints":[]}}' --type=merge
 
 clear 
-p "Case 1: DO NOT schedule workload to clusters with taint."
-pe "kubectl get managedclusters"
-pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec.taints}{\"\\n\"}{end}'"
-pe "cat placement1.yaml"
-pe "kubectl apply -f placement1.yaml -n ${NS}"
-pe "kubectl get placement -n ${NS}"
-pe "kubectl get placementdecision -n ${NS}"
-p ""
-
-p "Case 2: User could use toleration to schedule workload to clusters with taint."
-pe "cat placement1-toleration.yaml"
-pe "kubectl apply -f placement1-toleration.yaml -n ${NS}"
-pe "kubectl get placement -n ${NS}"
-pe "kubectl get placementdecision -n ${NS}"
-pe "kubectl describe placementdecision demo1-decision-1 -n ${NS} | grep Status -A 10"
-p ""
-
-clear 
-p "Case 3: User workload could be transferred to another available managed cluster after a tolerated time."
-p "Clear the taint"
-kubectl patch managedcluster ${c1} -p '{"spec":{"taints":[]}}' --type=merge
-kubectl patch managedcluster ${c2} -p '{"spec":{"taints":[]}}' --type=merge
+p "User Case: User workload could be transferred to another available managed cluster after a tolerated time."
+#p "Clear the taint"
 pe "kubectl get managedclusters -A -o=jsonpath='{range .items[*]}{.metadata.name}{\"\\t\"}{.spec.taints}{\"\\n\"}{end}'"
 
 pe "cat placement2.yaml"
