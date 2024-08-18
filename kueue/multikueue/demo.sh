2acc43366702a076814c288f5ecb76c75f1186a2
haoqing0110 <qhao@redhat.com>
2024-08-18 15:10:53 +0000

    update demo
    Signed-off-by: haoqing0110 <qhao@redhat.com>

--- kueue/multikueue/demo.sh
+++ kueue/multikueue/demo.sh
@@ -30,6 +30,12 @@ kubectl delete -f ./
 clusteradm clusterset unbind spoke --namespace kueue-system
 kubectl delete multikueueconfig placement-demo2
 kubectl delete multikueuecluster placement-demo2-cluster2 placement-demo2-cluster3
+kubectl delete job --context kind-hub $(kubectl get job --context kind-hub | awk '{print $1}')
+kubectl delete job --context kind-cluster1 $(kubectl get job --context kind-cluster1 | awk '{print $1}')
+kubectl delete job --context kind-cluster2 $(kubectl get job --context kind-cluster2 | awk '{print $1}')
+kubectl delete job --context kind-cluster3 $(kubectl get job --context kind-cluster3 | awk '{print $1}')
+kubectl delete pod --context kind-hub $(kubectl get pods --context kind-hub | awk '{print $1}')
+kubectl delete pod --context kind-cluster1 $(kubectl get pods --context kind-cluster1 | awk '{print $1}')
 kubectl delete pod --context kind-cluster2 $(kubectl get pods --context kind-cluster2 | awk '{print $1}')
 kubectl delete pod --context kind-cluster3 $(kubectl get pods --context kind-cluster3 | awk '{print $1}')
 clear 
@@ -69,40 +75,43 @@ clear
 #kubectl delete -f multikueue-setup-demo1.yaml >/dev/null 2>&1
 #clear 
 #
-p "Demo2: Leverage OCM's advanced scheduling features with MultiKueue to intelligently place AI/ML jobs across clusters."
-
-p "OCM implement an admissioncheck controller to automate the MultiKueue setup process, MultiKueueConfigs and MultiKueueClusters
-are generated dynamically based on OCM placement decisions."
-
-p "Demo2-1: Set MultiKueue for clusters with GPU type nvidia-tesla-t4."
+#p "Demo2: Leverage OCM's advanced scheduling features with MultiKueue to intelligently place AI/ML jobs across clusters."
+p "Demo2-1: Deploy Kueue workloads (data analysis or AI/ML training jobs) on clusters with GPU type nvidia-tesla-t4."
 
 p "OCM is installed with 1 hub cluster and 3 managed clusters."
-pe "kubectl get clusterprofile -A"
+pe "kubectl get managedclusters"
 
 p "Kueue is also installed on these clusters."
 pe "kubectl get pods -n kueue-system --context kind-hub"
 pe "kubectl get pods -n kueue-system --context kind-cluster1"
 p "cluster2 and cluster3 are the same, skip checking."
 
-p "vim ./multikueue-setup-demo2.yaml"
-pe "cat ./placement-demo2-1.yaml"
+p "The clusterprofile status contains the credential to be consumed by Kueue."
+pe "kubectl get clusterprofile -A"
+pe "kubectl get clusterprofile -A -ojson | jq '.items[] | .metadata.name, .status.credentials[]'"
 
+p "Only cluster2 and cluster3 have 3 GPU with type nvidia-tesla-t4."
+pe "kubectl get managedcluster --show-labels"
+pe "kubectl get managedcluster -ojson |jq '.items[] |.metadata.name, .status.allocatable[\"nvidia.com/gpu\"]'"
+
+p "Create placement to select clusters with GPU type nvidia-tesla-t4."
+pe "cat ./placement-demo2-1.yaml"
 pe "clusteradm clusterset bind spoke --namespace kueue-system"
 pe "clusteradm get clustersets"
 pe "kubectl apply -f ./placement-demo2-1.yaml"
 pe "clusteradm get placements -otable"
-p "Check the managed cluster labels, only cluster2 and cluster3 have nvidia-tesla-t4 label."
-pe "kubectl get clusterprofile -A -ojson |jq '.items[] |.metadata.name, (.status.properties[] | select(.name == "accelerator"))'"
 
+p "Use this placement when creating Multikueue environment."
+pe "cat ./multikueue-setup-demo2.yaml | less"
 pe "kubectl apply -f ./multikueue-setup-demo2.yaml"
 pe "kubectl get multikueueconfig"
 pe "kubectl get multikueueconfig -oyaml"
 pe "kubectl get multikueuecluster"
-pe "kubectl get multikueuecluster -A -ojson | jq '.items[] | .metadata.name, .status.conditions'"
+pe "kubectl get multikueuecluster -oyaml"
 pe "kubectl get admissionchecks -ojson | jq '.items[] | .metadata.name, .status.conditions'"
 pe "kubectl get clusterqueues -ojson | jq '.items[] | .metadata.name, .status.conditions'"
 
-p "Create a job to the MultiKueue."
+p "Create a job to this MultiKueue environment."
 pe "cat ./job-demo2.yaml"
 pe "kubectl create -f ./job-demo2.yaml"
 p "Check the workload on the managed clusters."
@@ -112,22 +121,20 @@ pe ""
 
 clear 
 
-p "Demo2-2: Set MultiKueue dynamically based on cluster avaliable GPU."
+p "Demo2-2: Deploy Kueue workloads (data analysis or AI/ML training jobs) on clusters with most available GPU dynamically."
 
-p "More advanced multicluster scheduling features can bring to MultiKueue."
-p "For example, with OCM resource usage collect addon installed, it scores clusters based on available GPU resource. With the resource changes, the selected clusters change."
-p "With the change, MultiKueue can be set up dynamically to smart place AI/ML jobs to the clusters with most available GPU resources."
+p "With the OCM Resource Usage Collect addon installed, clusters are scored based on their available GPU resources."
+p "As GPU resource usage changes across clusters, the selected clusters update dynamically."
+p "Additionally, MultiKueue can be configured to intelligently place AI/ML jobs on clusters with optimal GPU availability."
 
 p ""
-p "First, how the resource usage collect addon works?"
+p "First, how does the resource usage collection addon work?"
 p "The resource usage collect addon agent is already installed."
-pe "kubectl get mca -A"
+pe "kubectl get managedclusteraddon -A"
 pe "kubectl get pods -n open-cluster-management-agent-addon --context kind-cluster1"
 p "cluster2 and cluster3 are the same, skip checking."
 
 p "The cluster with more avaliable GPU has a higher addonplacementscore, score range is -100 to 100."
-p "In this env, cluster1 has no GPU, cluster2 and cluster3 has 3 GPUs."
-pe "kubectl get mcl -ojson | jq '.items[] | .metadata.name, .status.allocatable, .status.capacity'"
 p "One the clusters with GPU, there's one workload running."
 pe "kubectl get workload --context kind-cluster2"
 pe "kubectl get workload --context kind-cluster3"
